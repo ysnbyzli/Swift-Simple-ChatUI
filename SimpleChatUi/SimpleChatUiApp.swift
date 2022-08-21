b3da03e658c1ef0986be65bbcce4bcd598f80565
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2022-08-21 10:01:29 +0300

    Initial Commit

---
+++ SimpleChatUi/SimpleChatUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleChatUiApp.swift
+//  SimpleChatUi
+//
+//  Created by Yasin Beyazlı on 21.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleChatUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
