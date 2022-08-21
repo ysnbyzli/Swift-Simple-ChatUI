54b680b98fec84d519007cfea63a1a0fdffe975c
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2022-08-21 11:34:47 +0300

    feat: created calls view

--- SimpleChatUi/Views/Calls/CallsView.swift
+++ SimpleChatUi/Views/Calls/CallsView.swift
@@ -8,13 +8,76 @@
 import SwiftUI
 
 struct CallsView: View {
+    @State var searchText: String = ""
+    
+    let messages = [
+        ["user":"Shezad", "message": "Outgoing", "time": "1:30 PM"],
+        ["user":"Catherine", "message":"Incoming", "time": "4:25 PM"],
+        ["user":"Shezad", "message":"Incoming", "time": "5:00 PM"],
+        ["user":"Afna", "message": "Outgoing", "time": "8:45 PM"],
+        ["user":"Mathew", "message": "Outgoing", "time": "9:00 PM"],
+        ["user":"Afna", "message": "Incoming", "time": "Yesterday"],
+        ["user":"Jerin", "message": "Outgoing", "time": "Yesterday"],
+        ["user":"Shezad", "message":"Outgoing", "time": "Tuesday"],
+        ["user":"Afna", "message": "Incoming", "time": "Tuesday"],
+        ["user":"Mathew", "message": "Outgoing", "time": "Tuesday"],]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color("color_bg").edgesIgnoringSafeArea(.all)
+            VStack{
+                HStack{
+                    Text("Calls")
+                        .fontWeight(.semibold)
+                        .font(.largeTitle)
+                    Spacer()
+                    Image(systemName: "phone.badge.plus")
+                        .foregroundColor(Color("color_primary"))
+                        .font(.title2)
+                }
+                ScrollView(showsIndicators: false){
+                    VStack(alignment: .leading, spacing: 5) {
+                        SearchView(searchText: $searchText)
+                        VStack(spacing: 25){
+                            ForEach(messages, id: \.self){message in
+                                HStack{
+                                    Image(message["user"]!)
+                                        .resizable()
+                                        .background(Color("color_bg_inverted").opacity(0.05))
+                                        .frame(width: 60, height: 60, alignment: .center)
+                                        .clipShape(Circle())
+                                    VStack(alignment: .leading, spacing: 6){
+                                        HStack{
+                                            Text(message["user"]!)
+                                            Spacer()
+                                            Text(message["time"]!)
+                                        }
+                                        HStack{
+                                            Image(systemName: message["message"] == "Outgoing" ? "arrow.up.right" : "arrow.down.left")
+                                                .foregroundColor(message["message"] == "Outgoing" ? Color("color_primary") : .red)
+                                            
+                                            Text(message["message"]!).foregroundColor(Color("color_bg_inverted").opacity(0.5))
+                                                .lineLimit(2)
+                                        }
+                                    }
+                                    .padding(.horizontal, 10)
+                                }
+                            }
+                        }.padding(.top)
+                    }
+                }
+            }
+            .padding(.top)
+            .padding(.horizontal)
+        }
     }
 }
 
 struct CallsView_Previews: PreviewProvider {
     static var previews: some View {
-        CallsView()
+        Group{
+            CallsView().preferredColorScheme(.dark)
+            CallsView()
+        }
     }
 }
